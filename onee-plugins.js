6bd04711242a175b67c8cdf0b9518cd3b0ea39f5
chen <class4cxy@126.com>
2014-03-02 00:03:27 +0800

    Create onee-plugins.js

---
+++ onee-plugins.js
@@ -0,0 +1,118 @@
+;!function () {
+
+	if ( !onee ) return console && console.log && console.log("Base on onee.js");
+	
+	// is exist
+	if ( onee.plugins ) return;
+	
+	// 内置驱动/组件
+	// 说明：
+	// 基础目录 - Base/
+	// 组件目录 - Plugins/
+	// 键 - 目录名
+	// 值 - 需要加载的文件，具体与onee.inc接口一致
+
+
+    var ws = onee.workspace;
+    var base = ws + "Base/";
+    var Plugin = ws + "Plugin/";
+    var sizzle = base + "Sizzle/Sizzle.js";
+    var jquery = base + "jQuery/jquery.js";
+    var Tween = base + "Tween/Tween.js";
+    var RequestAnimationFrame = base + "Tween/RequestAnimationFrame.js";
+    var dom = base + "onee.dom/dom.js";
+    var ajax = base + "onee.ajax/ajax.js";
+	
+	onee.plugins = {
+
+		// Driver
+		"sizzle" : [
+			sizzle
+		],
+		"jquery" : [
+			jquery
+		],
+        "Tween" : [
+            RequestAnimationFrame,
+            Tween
+        ],
+		"onee.dom" : [
+			sizzle,
+			dom
+		],
+		"onee.ajax" : [
+			ajax
+		],
+
+		// Plugin
+		"onee.TmplM" : [
+			ajax,
+			Plugin + "onee.TmplM/TmplM.js"
+		],
+		"onee.form" : [
+			[sizzle, ajax],
+			Plugin + "onee.form/form.js"
+		],
+		"onee.Powin" : [
+			sizzle,
+			dom,
+			ajax,
+			[
+                Plugin + "onee.Powin/Powin.js",
+                Plugin + "onee.Powin/powin-style.css"
+            ]
+		],
+		"onee.swf" : [
+			sizzle,
+			dom,
+			Plugin + "onee.swf/swf.js"
+		],
+        "onee.scrollpx" : [
+            sizzle,
+            dom,
+            Plugin + "onee.scrollpx/index.js"
+        ],
+        "onee.scrollitem" : [
+            [
+                RequestAnimationFrame,
+                Tween,
+                sizzle
+            ],
+            dom,
+            Plugin + "onee.scrollitem/index.js"
+        ],
+        "onee.scrollanimation" : [
+            [
+                RequestAnimationFrame,
+                Tween,
+                sizzle
+            ],
+            dom,
+            Plugin + "onee.scrollitem/index.js",
+            Plugin + "onee.scrollanimation/index.js"
+        ],
+		"jquery.tab" : [
+			jquery,
+			Plugin + "jQuery.tab/Tab.js"
+		],
+		"jquery.unslider" : [
+			jquery,
+			Plugin + "jQuery.unslider/Unslider.js"
+		],
+        "jquery.3dslider" : [
+            jquery,
+            Plugin + "jQuery.3dslider/index.js"
+        ],
+        "jquery.lightbox" : [
+            jquery,
+            Plugin + "jQuery.lightbox/index.js"
+        ],
+        "slipjs" : [
+            sizzle,
+            dom,
+            Plugin + "mobile.slipjs/index.js"
+        ]
+
+	};
+
+} ();
